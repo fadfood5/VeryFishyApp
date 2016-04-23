df516e0cbbe2854aefb53597eabe0fd661452505
ProjectProgramAMark <projectprogramamark@gmail.com>
2016-04-23 00:21:59 -0400

    Implemented token authentication.

--- Server/api/controllers/AuthController.js
+++ Server/api/controllers/AuthController.js
@@ -6,47 +6,33 @@
  */
 
 module.exports = {
-	authenticate: function(req, res) {
+  index: function (req, res) {
     var email = req.param('email');
     var password = req.param('password');
 
     if (!email || !password) {
-      return res.json(401, {err: 'username and password required'});
+      return res.json(401, {err: 'email and password required'});
     }
 
-    User.findOneByEmail(email, function(err, user) {
+    Users.findOne({email: email}, function (err, user) {
       if (!user) {
-        return res.json(401, {err: 'invalid username or password'});
+        return res.json(401, {err: 'invalid email or password'});
       }
 
-      User.validPassword(password, user, function(err, valid) {
+      Users.comparePassword(password, user, function (err, valid) {
         if (err) {
           return res.json(403, {err: 'forbidden'});
         }
 
         if (!valid) {
-          return res.json(401, {err: 'invalid username or password'});
+          return res.json(401, {err: 'invalid email or password'});
         } else {
-          res.json({user: user, token: sailsTokenAuth.issueToken({sid: user.id})});
+          res.json({
+            user: user,
+            token: jwToken.issue({id : user.id })
+          });
         }
       });
     })
-  },
-
-  register: function(req, res) {
-    //TODO: Do some validation on the input
-    if (req.body.password !== req.body.confirmPassword) {
-      return res.json(401, {err: 'Password doesn\'t match'});
-    }
-
-    User.create({email: req.body.email, password: req.body.password}).exec(function(err, user) {
-      if (err) {
-        res.json(err.status, {err: err});
-        return;
-      }
-      if (user) {
-        res.json({user: user, token: sailsTokenAuth.issueToken({sid: user.id})});
-      }
-    });
   }
 };
--- Server/config/policies.js
+++ Server/config/policies.js
@@ -27,6 +27,14 @@ module.exports.policies = {
   ***************************************************************************/
 
   // '*': true,
+  '*': ['isAuthorized'], // Everything resctricted here
+  'UserController': {
+    'create': true // We dont need authorization here, allowing public access
+  },
+
+  'AuthController': {
+    '*': true // We dont need authorization here, allowing public access
+  }
 
   /***************************************************************************
   *                                                                          *
@@ -48,4 +56,13 @@ module.exports.policies = {
 		// before letting any users feed our rabbits
 		// feed : ['isNiceToAnimals', 'hasRabbitFood']
 	// }
+
+  /*
+ * For more information on how policies work, see:
+ * http://sailsjs.org/#/documentation/concepts/Policies
+ *
+ * For more information on configuring policies, check out:
+ * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.policies.html
+ */
+
 };

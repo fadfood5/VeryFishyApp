7747d48d49e72be29cc2aa84fcf3148a0d2717d7
ProjectProgramAMark <projectprogramamark@gmail.com>
2016-04-23 22:08:48 -0400

    Cleaned app side things up

--- App/www/js/app.js
+++ App/www/js/app.js
@@ -9,109 +9,6 @@ var apiBaseUrl = 'http://localhost:3000/data';
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic-material'])
 
-.controller('MapCtrl', function($scope, $ionicLoading, $compile, $ionicModal) {
-  function initialize() {
-    var myLatlng = new google.maps.LatLng(43.07493, -89.381388);
-
-    var mapOptions = {
-      center: myLatlng,
-      zoom: 16,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"),
-      mapOptions);
-
-    //Marker + infowindow + angularjs compiled ng-click
-    var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-    var compiled = $compile(contentString)($scope);
-
-    var infowindow = new google.maps.InfoWindow({
-      content: compiled[0]
-    });
-
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map, marker);
-    });
-
-    $scope.map = map;
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
-
-  $scope.centerOnMe = function() {
-    if (!$scope.map) {
-      return;
-    }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      alert(pos.coords.latitude);
-      $scope.loading.hide();
-    }, function(error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
-
-  $scope.clickTest = function() {
-    alert('Example of infowindow with ng-click')
-  };
-
-  $scope.addTrap = function() {
-    if (!$scope.map) {
-      return;
-    }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    var coord;
-
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      //$scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      coord = pos.coords.latitude.toString() + ' ' + pos.coords.longitude.toString();
-      alert(coord);
-    }, function(error) {
-      alert('Unable to get location: ' + error.message);
-    });
-
-  };
-
-  $ionicModal.fromTemplateUrl('my-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-      $scope.modal.hide();
-  };
-    // cleanup modal
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
-  // execute action on hide
-  $scope.$on('modal.hidden', function() {
-    // nothing for now
-  });
-  $scope.$on('modal.removed', function() {
-    // kill things
-  });
-});
 
 .run(function($ionicPlatform, $http) {
   $ionicPlatform.ready(function() {
@@ -167,6 +64,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
--- App/www/js/services.js
+++ App/www/js/services.js
@@ -1,31 +1,66 @@
 angular.module('starter.services', [])
 
-.service('FeedService', function($http, $q) {
-  var url = apiBaseUrl + '/feed';
-
-  return {
-    getFeed: function(success, failure) {
-
-    var deferred = $q.defer();
-     $http.get(url)
-       .success(function(res) {
-         //console.log("res is: ", res);
-         deferred.resolve(res);
-       }).error(function(msg, code) {
-          deferred.reject(msg);
-          // Using JSON stringify to get the console to actually show the message
-          console.log(JSON.stringify(msg), code);
-       });
-     return deferred.promise;
-
-    },
-    get: function(feedId) {
-      for (var i = 0; i < feeds.length; i++) {
-        if (feeds[i].id === parseInt(feedId)) {
-          return feeds[i];
+.service('LoginService', function($http, $q) {
+  var url = apiBaseUrl + '/login';
+    return {
+        loginUser: function(credentials, success, failure) {
+          // uses $q to turn function into a promise
+          // reasoning being so the client side can handle either success/failure
+          var deferred = $q.defer();
+          var promise = deferred.promise;
+          $http.post(url, {'email': credentials.email, 'password': credentials.password}).then(function(res) {
+            //console.log(JSON.stringify(res));
+            if((typeof res.data.token !== 'undefined')) {
+              console.log("deferred is RESOLVED");
+              deferred.resolve( { token: res.data.token } );
+              window.localStorage.token = res.data.token;
+              $http.defaults.headers.common.x_access_token = window.localStorage.token;
+              //console.log("the http default should be saved as: ", $http.defaults.headers.common.x_access_token);
+              //console.log("The token should now be saved as", window.localStorage.token);
+            } else {
+              //console.log("deferred is REJECTED");
+              deferred.reject( { error: 'invalid_response' } );
+            }
+          }, function(err) {
+              if((typeof result.data.error !== 'undefined')) {
+                  deferred.reject( { error: err.data.error, status: err.status } );
+              } else {
+                deferred.reject( { error: 'invalid_login' } );
+              }
+          });
+          return promise;
+              // window.localStorage.token = res.data.token;
+              // console.log("The token should now be saved as", window.localStorage.token);
+              // return res;
+            // }, function(err) {
+            //   console.log(JSON.stringify(err));
+            // });
         }
-      }
-      return null;
-    }
-  };
+    };
+})
+
+.service('RegisterService', function($http, $q) {
+  var url = apiBaseUrl + '/newUser';
+  return {
+    registerUser: function(data, success, failure) {
+        var deferred = $q.defer();
+        var promise = deferred.promise;
+        $http.post(url, {
+          'email': data.email,
+          'username': data.username,
+          'password': data.password
+        }).then(function(res) {
+          if(data.email && data.username && data.password) {
+            deferred.resolve("registration successful!");
+          } else {
+            deferred.reject({ error: 'invalid registration request' });
+          }
+        }, function(err) {
+            deferred.reject({ error: err });
+          });
+          return promise;
+        }
+    };
+  })
+
 });
